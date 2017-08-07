a1b635d011cc1f75d32ef2fab1c9136c7f1e07e1
Gyuchang Jun <gyuchang.jun@gmail.com>
2017-08-07 01:16:57 -0700

    fix build scripts:
     - configure: typo
     - bootstrap.sh: remove lua related flags. configure fix will make the override unnecessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 base=$(cd "$(dirname "$0")"; pwd)
 cd $base
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-PKG_CONFIG_PATH=/usr/local/lib/pkgconfig ./configure --with-mysql=/usr --prefix=/usr/local/mysql-proxy CFLAGS="-DHAVE_LUA_H -O2" LDFLAGS="-lm -ldl -lcrypto -ljemalloc" LUA_CFLAGS="-I/usr/local/include/" LUA_LIBS="-L/usr/local/lib -llua"
+PKG_CONFIG_PATH=/usr/local/lib/pkgconfig ./configure --with-mysql=/usr --prefix=/usr/local/mysql-proxy CFLAGS="-O2" LDFLAGS="-lm -ldl -lcrypto -ljemalloc"
